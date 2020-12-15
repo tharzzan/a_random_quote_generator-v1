b64813893c63ebe2e7ac506a55278e6f42a08c30
Andy Hartono <andy.hartono@outlook.com>
2020-12-16 00:54:30 +0700

    done with this project, ready to be graded

---
+++ README.md
@@ -0,0 +1,3 @@
+# Treehouse Project
+
+generate webpage that shows random quotes when a button is clicked
--- js/script.js
+++ js/script.js
@@ -3,26 +3,104 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
 /*** 
- * `quotes` array 
+ * `quotes` array, contains objects with the following properties:
+ * quote, source, citation, and year
 ***/
+const quotes = [
+  {
+    quote: 'Every master was once a disaster.',
+    source: 'T. Harv Eker',
+    tags: 'Motivation'
+  },
+  {
+    quote: 'Never doubt that a small group of thoughtful, committed, citizens can change the world. Indeed, it is the only thing that ever has.',
+    source: 'Margaret Mead'
+  },
+  {
+    quote: 'The young do not know enough to be prudent, and therefore they attempt the impossible - and achieve it, generation after generation.',
+    source: 'Pearl S. Buck'
+  },
+  {
+    quote: 'If you want to go fast, go alone. If you want to go far, go together.',
+    source: 'African Proverb',
+    tags: 'Wisdom'
+  },
+  {
+    quote: `One of the toughest job for entrepreneur is realizing that they are entrepreneur, they are not doers. They should not be micro managing. It means finding delicate balance in leading, empowering, keeping in touch, giving feedback. It's much harder than doing it yourself. But you have to go for the harder one. Because the second, third and fourth, they'll begin to do it better than you. By concentrating on leadi​ng, I'm one of the better leaders of our company.`,
+    source: 'Tom Phillips',
+    tags: 'Business'
+  },
+  {
+    quote: `Dream high... as high as the sky.\nIf you fall, you'll fall among the stars`,
+    source: `Soekarno (Indonesia's 1st President)`
+  },
+  {
+    quote: 'Big dreams often upset people with small minds.',
+    source: 'Brad Sugars',
+    citation: 'Facebook',
+    year: '2019'
+  },
+  {
+    quote: 'A sign of a good leader is not how many followers you have, but how many leaders you create.',
+    source: 'Mahatma Gandhi'
+  }
+]
 
 
+/**
+ * generate random quotes from provided array of Quotes
+ * @param {array} arrOfQuotes - array of quotes
+ * @returns {object} - random quote object 
+ */
+function getRandomQuote(arrOfQuotes) {
+  const quoteNo = Math.floor(Math.random() * arrOfQuotes.length) // will create quote index No., from 0 to (length-1)
 
-/***
- * `getRandomQuote` function
-***/
+  return arrOfQuotes[quoteNo]
+}
+
+function getRandomBackgroundColor() {
+  let randomColor = []
+
+  for (let i = 0; i < 3; i++) {
+
+    randomColor.push(Math.floor(Math.random() * 255) + 1)
+  }
+
+  return `rgb(${randomColor[0]}, ${randomColor[1]}, ${randomColor[2]})`
+}
 
 
+/**
+ * generate HTML that shows random quote
+ */
+function printQuote() {
+  const quoteToShow = getRandomQuote(quotes) // --> object
+  let quoteHtml = `
+    <p class="quote">${quoteToShow.quote}</p>
+    <p class="source">${quoteToShow.source}
+  `
 
-/***
- * `printQuote` function
-***/
+  if (quoteToShow.citation) {
+    quoteHtml += `<span class="citation">${quoteToShow.citation}</span>`
+  }
 
+  if (quoteToShow.year) {
+    quoteHtml += `<span class="year">${quoteToShow.year}</span>`
+  }
+
+  if (quoteToShow.tags) {
+    quoteHtml += `<span class="tags">(${quoteToShow.tags})</span>`
+  }
+
+  // finish the quoteHtml by adding the closing </p>
+  quoteHtml += `</p>`
+
+  // change the background color
+  document.querySelector('body').style.backgroundColor = getRandomBackgroundColor()
+
+  document.getElementById('quote-box').innerHTML = quoteHtml
+}
 
 
 /***
@@ -31,3 +109,6 @@ project 1 - A Random Quote Generator
 ***/
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
+
+// auto-refresh quotes every 10 seconds
+let autoRefresh = window.setInterval(printQuote, 10000)
